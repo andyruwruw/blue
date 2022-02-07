751e0abe7b5a6346eb75c965dc96c10a2a9c7af0
Andrew Young <46572910+andyruwruw@users.noreply.github.com>
2022-02-06 16:47:36 -0800

    Ignore Files

--- server/dist/data/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataManager = void 0;
-var data_manager_1 = require("./data-manager");
-Object.defineProperty(exports, "DataManager", { enumerable: true, get: function () { return data_manager_1.DataManager; } });
-//# sourceMappingURL=index.js.map
